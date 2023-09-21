8015a8f3f50ce7ce4143d70fc5f787fafa5fe04a
= <=>
2023-09-21 14:12:33 +0530

    ch01 complete

--- ch01/main.py
+++ ch01/main.py
@@ -1,12 +1,10 @@
 import numpy as np
 import cv2
 
-img = cv2.imread("./assets/soubhik.png",0)
-# cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
-# cv2.imshow("image",img)
-# cv2.waitKey(0)
-# cv2.imwrite("output.jpg",img)
-with open("pixels.txt","w")as fp:
-    fp.write(str(img))
-    fp.close()
-
+img = cv2.imread("./assets/download.jpg",0)
+img = cv2.resize(img,(400,400))
+img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+img = cv2.rotate(img, cv2.ROTATE_90_COUNTERCLOCKWISE)
+cv2.imshow("image",img)
+cv2.waitKey(0)
+cv2.namedWindow("Image",cv2.WINDOW_NORMAL)
